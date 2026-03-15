498bd0aa107f66a842bd620177a48c333b6e96f0
Mateus Evaristo do Lago <mateusevaristolago@gmail.com>
2026-03-14 21:19:17 -0300

    add agentePython

---
+++ backend/monitor-agent/agent.py
@@ -0,0 +1,55 @@
+import requests
+import time
+
+API_URL = "http://localhost:5232/api"
+CHECK_INTERVAL = 30
+
+
+def get_services():
+    response = requests.get(f"{API_URL}/services")
+    return response.json()
+
+
+def send_check(data):
+    requests.post(f"{API_URL}/checks", json=data)
+
+
+def check_service(service):
+
+    url = service["url"]
+    service_id = service["id"]
+
+    start = time.time()
+
+    try:
+        response = requests.get(url, timeout=10)
+        is_online = response.status_code < 500
+    except:
+        is_online = False
+
+    response_time = int((time.time() - start) * 1000)
+
+    return {
+        "serviceId": service_id,
+        "isOnline": is_online,
+        "responseTimeMs": response_time
+    }
+
+
+while True:
+
+    print("Buscando serviços...")
+
+    services = get_services()
+
+    for service in services:
+
+        result = check_service(service)
+
+        print("Resultado:", result)
+
+        send_check(result)
+
+    print("Aguardando próximo ciclo...")
+
+    time.sleep(CHECK_INTERVAL)
