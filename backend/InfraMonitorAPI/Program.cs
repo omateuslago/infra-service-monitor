56570fbcf79d5d098bcb495c2034ee9611df2e03
Mateus Evaristo do Lago <mateusevaristolago@gmail.com>
2026-03-28 01:31:34 -0300

    mudanças no dockercompose, sql

--- backend/InfraMonitorAPI/Program.cs
+++ backend/InfraMonitorAPI/Program.cs
@@ -5,7 +5,10 @@ using InfraMonitorAPI.Database;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        sql => sql.EnableRetryOnFailure()
+));
 
 // services
 builder.Services.AddControllers();
@@ -13,6 +16,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// aplica migrations automaticamente ao iniciar
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 // swagger
 app.UseSwagger();
